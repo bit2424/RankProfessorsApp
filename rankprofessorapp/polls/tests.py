fdd06ccffafa30e83c5b013a42de120bec88b384
Nelson David Quiñones Virgen <44851531+bit2424@users.noreply.github.com>
2022-09-28 15:28:59 -0500

    Exploration of the database communication

--- rankprofessorapp/polls/tests.py
+++ rankprofessorapp/polls/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from polls.models import Choice, Question
+from django.utils import timezone
+q = Question(question_text = "LOL?" , pub_date = timezone.now())
+q.save() 
+q2  = Question.objects.create(question_text="¿Cuál es el mejor curso de platzi?" , pub_date=timezone.now()) 
+
+
