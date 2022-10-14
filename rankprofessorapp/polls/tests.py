5795404b7dc77e01a6072cbba65353b078bfb060
Nelson David Quiñones Virgen <44851531+bit2424@users.noreply.github.com>
2022-10-14 06:20:05 -0500

    Dummy application  to test Django framework

--- rankprofessorapp/polls/tests.py
+++ rankprofessorapp/polls/tests.py
@@ -6,5 +6,3 @@ from django.utils import timezone
 q = Question(question_text = "LOL?" , pub_date = timezone.now())
 q.save() 
 q2  = Question.objects.create(question_text="¿Cuál es el mejor curso de platzi?" , pub_date=timezone.now()) 
-
-
